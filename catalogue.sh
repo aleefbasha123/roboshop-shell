e3cfcdd0b867ef52ac95ed6e63e8c0b93ee4b50d
aleefbasha123 <aleefbasha123@gmail.com>
2025-02-09 17:17:37 +0530

    mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -29,52 +29,52 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 VALIDTAE $? "Disable nodejs"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> $LOGFILE
 VALIDATE $? "Enable Nodejs"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGFILE
 VALIDTAE $? "Insatlling Nodejs"
 
-useradd roboshop
+useradd roboshop &>> $LOGFILE
 VALIDATE $? "Adding roboshop user"
 
-mkdir /app
+mkdir /app &>> $LOGFILE
 VALIDATE $? "Cretaing ap directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 VALDIATE $? "Downloading catalogue application"
 
-cd /app 
+cd /app &>> $LOGFILE
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> $LOGFILE
 VALIDATE $? "Unzipping catalogue application"
 
-npm install 
+npm install  &>> $LOGFILE
 VALIDATE $? "Insatting depedences"
 
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
 VALIDATE $? "Coping catalogue services"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
 VALDIATE $? "catalogie deamon reload"
 
-systemctl enable catalogue
+systemctl enable catalogue &>> $LOGFILE
 VALIDATE $? "catalogue enable "
 
-systemctl start catalogue
+systemctl start catalogue &>> $LOGFILE
 VALIDATE $? "starting catalogue"
 
 
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? "coping mongo repo"
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "insatlling mongodb-org-shell"
 
-mongo --host 172.31.36.138 </app/schema/catalogue.js
+mongo --host 172.31.36.138 </app/schema/catalogue.js &>> $LOGFILE
 VALIDATE $? "Loading catalogue data inot mongobd"
 
 
