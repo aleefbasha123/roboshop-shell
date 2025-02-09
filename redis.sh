d737513ab4ed83c291a82e95c2b5c56ba5527e7e
aleefbasha123 <aleefbasha123@gmail.com>
2025-02-09 20:22:37 +0530

    mongodb

--- redis.sh
+++ redis.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>$LOGFILE
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
@@ -29,20 +30,27 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
+
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-VALIDATE $? "Insatlling remi"
+
+VALIDATE $? "Installing Remi release"
 
 dnf module enable redis:remi-6.2 -y
-VALIDATE $? "Enable redis"
+
+VALIDATE $? "enabling redis"
 
 dnf install redis -y
-VALIDATE $? "Insatlling redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
-VALIDATE $? "chanining acess"
+VALIDATE $? "Installing Redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+
+VALIDATE $? "allowing remote connections"
 
 systemctl enable redis
-VALIDATE $? "Enable redis"
+
+VALIDATE $? "Enabled Redis"
 
 systemctl start redis
-VALIDATE $? "starting redis"
+
+VALIDATE $? "Started Redis"
